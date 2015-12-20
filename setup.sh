3ace383a770bc6e79d16a9c69f7d2d79e21e660c
童中波 <easytzb@gmail.com>
2015-12-20 09:53:41 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+git clone git@github.com:easytzb/vim.git ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 if [ $SHELL = "/bin/bash" ]; then
 	echo 'alias vim="vim -u ~/.vim/vimrc"'>>~/.bash_profile
 elif [ $SHELL = "/bin/zsh" ]; then
+	echo 'alias vim="vim -u ~/.vim/vimrc"'>>~/.zshrc
+elif [ $SHELL = "/usr/local/bin/zsh" ]; then
 	echo 'alias vim="vim -u ~/.vim/vimrc"'>>~/.zshrc
 else 
 	cp ./vimrc ~/.vimrc
